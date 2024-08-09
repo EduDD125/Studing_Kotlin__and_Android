e835365643ec93097d328eb225b60fa6ee855209
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-09 15:52:31 -0300

    feat[Main]: meu primeiros códigos manipulando string e imprimido resultados

---
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main (args: Array<String>) {
+            var nome: String = "Eduardo "
+            nome += "Derisso"
+
+            println(nome);
+        }
+    }
+}
