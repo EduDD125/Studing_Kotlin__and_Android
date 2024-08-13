68dc36208425491208b5ba71983cb244828c89e2
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-12 21:22:52 -0300

    feat[MainActivity]: created my first andorid app - a counter of clicks

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.contador
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cliqueBt: Button
+    private lateinit var contadorTv: TextView
+    private var contador: Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main) // depois dessa função ocorre a criação dos objetos
+                                               // que vamos querer referencias de dentro de resources
+
+        cliqueBt = findViewById(R.id.cliqueBt)
+        contadorTv = findViewById(R.id.contadorTv)
+
+        cliqueBt.setOnClickListener {
+            contadorTv.setText((++contador).toString());
         }
     }
 }
